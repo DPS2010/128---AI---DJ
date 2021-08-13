b94eb8c4dcbeb043673809f3575d974c0aa8375f
DPS2010 <77051637+DPS2010@users.noreply.github.com>
2021-08-13 15:24:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+leftWristx = 0
+rightWristx = 0
+leftWristy = 0
+rightWristx = 0
+song = "";
+function preload() {
+    song = loadSound("music.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(600,500)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose',gotPoses)
+}
+function draw() {
+ image(video, 0, 0, 600, 500)
+}
+function modelLoaded() {
+    console.log("Model loaded")
+
+}
+function play() {
+    song.play()
+    song.setVolume(1)
+    song.rate(1)
+}
+function gotPoses(result) {
+    if (result.length > 0) {
+        console.log(result)
+        leftWristx = result[0].pose.leftWrist.x
+        leftWristy = result[0].pose.leftWrist.y
+        rightWristx = result[0].pose.rightWrist.x
+        rightWristy = result[0].pose.rightWrist.y
+        console.log("Left Wrist X = " + leftWristx + " Right Wrist X = " + rightWristx + " Left Wrist Y = " + leftWristy + " Right Wrist Y" + rightWristy )
+        
+    }
+} 
